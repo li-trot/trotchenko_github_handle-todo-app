ba3efd0e5993bb1151b6b632f4a2a8644dc7f276
Lidiia Trotchenko <li.trot.it@gmail.com>
2022-12-08 10:00:20 +0100

    Additional check method. Print all task with mark done/undone

--- todo.py
+++ todo.py
@@ -15,7 +15,7 @@ def read_file():
 
 
 def list_items():
-    """List all tasks.
+    """Lists all tasks.
     If there is no tasks prints message."""
     tasks = read_file()
     if len(tasks) == 0:
@@ -28,29 +28,56 @@ def list_items():
 
 
 def add_task(new_tasks):
-    """Function for adding task to the todo.txt.
-    Argument - str - task."""
+    """Function for adding tasks to the todo.txt.
+    Argument - list - task or list of tasks."""
     with open("todo.txt", "a", encoding="utf-8") as file:
         for idx_t in new_tasks:
-            file.write(idx_t)
-            file.write("\n")
+            file.write("[ ] " + idx_t + "\n")
 
 
 def remove_task(number):
-    """Remove task from list of tasks with particular index.
-    Argument - number."""
+    """Removes task from list of tasks with particular index.
+    Argument - number.
+    Raises Error -  if provided arguments aren't appropriate numbers.
+    """
+    list_to_del = set()
+    tasks = read_file()
     try:
-        num_del = int(number)
-        tasks = read_file()
-        if len(tasks) < num_del:
-            print("Unable to remove: index is out of bound")
-        else:
-            with open("todo.txt", "w", encoding="utf-8") as file:
-                for num, line in enumerate(tasks):
-                    if num != (num_del - 1):
-                        file.write(line+"\n")
+        for each_num in number:
+            num_del = int(each_num) - 1
+            if len(tasks) < (num_del + 1):
+                print(f"Unable to remove: {each_num} index is out of bound")
+            else:
+                list_to_del.add(num_del)
+        with open("todo.txt", "w", encoding="utf-8") as file:
+            for num, line in enumerate(tasks):
+                if num not in list_to_del:
+                    file.write(line+"\n")
     except ValueError:
         print("Unable to remove: index is not a number")
+
+
+def check_task(done):
+    """To mark the task like done.
+    Argument - number of line.
+    Raises Error -  if provided arguments aren't appropriate numbers.
+    """
+    list_to_check = set()
+    tasks = read_file()
+    try:
+        for each_task in done:
+            num_check = int(each_task) - 1
+            if len(tasks) < (num_check + 1):
+                print(f"Unable to check: {each_task} index is out of bound")
+            else:
+                list_to_check.add(num_check)
+        with open("todo.txt", "w", encoding="utf-8") as file:
+            for num, line in enumerate(tasks):
+                if num in list_to_check:
+                    line = line.replace("[ ]", "[x]")
+                file.write(line+"\n")
+    except ValueError:
+        print("Unable to check: index is not a number")
 
 
 def info():
@@ -60,37 +87,45 @@ def info():
 =============================
 
 Command line arguments:
--l   Lists all the tasks
--a   Adds a new task
--r   Removes an task
--c   Completes an task""")
+-l              Lists all the tasks
+-a "todo task"  Adds a new task
+-r _num_...     Removes an task
+-c _num_...     Completes an task""")
     sys.stdout.buffer.write(text.encode("utf8"))
 
 
 if __name__ == "__main__":
     args = sys.argv
     try:
-        if args[0] == "todo.py":
-            if len(args) == 1:
-                globals()["info"]()
-            elif args[1] not in ["-a", "-l", "-r", "-c"]:
-                print("Unsupported argument \n")
-                globals()["info"]()
-            elif args[1] == "-l":
-                args[1] = "list_items"
-                globals()[args[1]]()
-            elif args[1] == "-a":
-                args[1] = "add_task"
-                if len(args) < 3:
-                    print("Unable to add: no task provided")
-                else:
-                    globals()[args[1]](args[2:])
-            elif args[1] == "-r":
-                args[1] = "remove_task"
-                if len(args) < 3:
-                    print("Unable to remove: no index provided")
-                else:
-                    globals()[args[1]](args[2])
+        # print information
+        if len(args) == 1:
+            globals()["info"]()
+    # wrong argument - print information
+        elif args[1] not in ["-a", "-l", "-r", "-c"]:
+            print("Unsupported argument \n")
+            globals()["info"]()
+    # list tasks
+        elif args[1] == "-l":
+            args[1] = "list_items"
+            globals()[args[1]]()
+    # add tasks
+        elif args[1] == "-a":
+            args[1] = "add_task"
+            if len(args) < 3:
+                print("Unable to add: no task provided")
+            else:
+                globals()[args[1]](args[2:])
+    # remove index task
+        elif args[1] == "-r":
+            args[1] = "remove_task"
+            if len(args) < 3:
+                print("Unable to remove: no index provided")
+            else:
+                globals()[args[1]](args[2:])
+    # done task
+        elif args[1] == "-c":
+            args[1] = "check_task"
+            globals()[args[1]](args[2:])
 
     except IndexError:
-        pass
+        print("Error")
